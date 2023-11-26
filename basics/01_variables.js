0cf8e6afdbad61a0d20af5d089b222455017eee7
PreetRX200 <145206362+PreetRX200@users.noreply.github.com>
2023-11-26 13:27:56 +0000

    hi

---
+++ basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 146522
+let accountEmail = "Ryan@google.com"
+var accountPassword = "32524" // Var is not used in modern js because of block scope and functional scope
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 not allowed to change constant 
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState ]);
+
