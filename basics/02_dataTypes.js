1ce862fd85fc7fddec0a6fdd789920f43f5f9e54
PreetRX200 <145206362+PreetRX200@users.noreply.github.com>
2023-11-26 14:00:30 +0000

    data Types

---
+++ basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict" // treat all js code as newer version
+
+console.log(3 + 3);
+
+let name = "Satoru Gojo"
+let age = 21
+let isLoggedIn = false
+let state = null
+
+/* Data Types
+ number => 2 to power 53
+ bigint
+ string => ""
+ boolean => true/false
+ null => standalone value
+ undefined => 
+ symbol => unique
+ object
+ 
