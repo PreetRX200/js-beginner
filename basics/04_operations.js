b671955f6baefefcfa4fba7fef4116818a1523cc
PreetRX200 <145206362+PreetRX200@users.noreply.github.com>
2023-11-28 06:43:10 +0000

    MDN & ECMA SCRIPT

---
+++ basics/04_operations.js
@@ -0,0 +1,18 @@
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+// Refer ECMA script for operator precedence and data types
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log(+true);
+console.log(+"");
+
+// Refer MDN js for prefix postfix increment
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
