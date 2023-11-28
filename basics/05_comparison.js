0439fe9d498c809ed0fa93e21cd74db96c5e6d75
PreetRX200 <145206362+PreetRX200@users.noreply.github.com>
2023-11-28 07:02:17 +0000

    comparison

---
+++ basics/05_comparison.js
@@ -0,0 +1,22 @@
+/*  basic comparison
+    console.log(2 > 1);
+    console.log(2 >= 1);
+    console.log(2 < 1);
+    console.log(2 == 1);
+    console.log(2 != 1);
+*/
+/* string comparison
+    console.log("2" > 1);
+    console.log("02" > 1);
+*/
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === strict check also check if strictly same data type or not then operator check
+
+console.log("2" === 2);
